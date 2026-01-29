ecf4dfc127f44b1ed2ada403138dfe6851cd1142
Aashbin Sunar <aashbinsunar1@gmail.com>
2026-01-30 03:52:40 +0545

    feat: add payment gateway column to tickets table with default value

---
+++ migrations/000003_add_payment_gateway_to_tickets.down.sql
@@ -0,0 +1,12 @@
+-- +migrate Up
+ALTER TABLE tickets DROP COLUMN payment_gateway;
+
+-- +migrate Down
+-- Add payment_gateway column as nullable first
+ALTER TABLE tickets ADD COLUMN payment_gateway VARCHAR(50);
+
+-- Update existing records with default value 'cash'
+UPDATE tickets SET payment_gateway = 'cash' WHERE payment_gateway IS NULL;
+
+-- Make the column NOT NULL
+ALTER TABLE tickets ALTER COLUMN payment_gateway SET NOT NULL;
