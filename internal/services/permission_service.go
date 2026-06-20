79a1ef1b88f83e1da8c12e8eaac4073a64288de8
Aashbin Sunar <aashbinsunar1@gmail.com>
2026-06-21 00:46:50 +0545

    init of permission for staff also

--- internal/services/permission_service.go
+++ internal/services/permission_service.go
@@ -177,7 +177,7 @@ func (s *PermissionService) InitializeSystemRolesSafely() error {
 				"create:ticket", "read:ticket", "scan:ticket", "checkin:ticket",
 			},
 			"staff": {
-				"read:ticket", "scan:ticket", "checkin:ticket",
+				"read:ticket", "scan:ticket", "checkin:ticket", "view:profile",
 			},
 			"user": {
 				"view:profile", "update:profile",
